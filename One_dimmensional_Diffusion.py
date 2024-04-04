6a76a92c60c3c0d8c5c16f703cfb21dc3c46c172
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2024-04-04 14:39:59 -0500

    Update One_dimmensional_Diffusion.py

--- One_dimmensional_Diffusion.py
+++ One_dimmensional_Diffusion.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters 
-nt = 200    # number of time steps to simulate
-nx = 30    # number of points to simulate
-dt = 0.0001
+nt = 500    # number of time steps to simulate
+nx = 100    # number of points to simulate
+dt = 0.00001
 
 def k(u, x):
     return 1.0
@@ -12,10 +12,10 @@ def k(u, x):
 def dk(u, x):
     return 0.0
 
-def populate_matrix(A, u, t, dx, nx):
+def populate_matrix(A, u, x, t, dx, nx):
     for i in range(nx):
-        d1 = dk(u[i], t)*dx - k(u[i], t)
-        d2 = dk(u[i], t)*dx + k(u[i], t)
+        d1 = dk(u[i], x[i])*dx - 2.0*k(u[i], x[i])
+        d2 = dk(u[i], x[i])*dx + 2.0*k(u[i], x[i])
         
         if i == 0:
             A[0][0:4] = [0.0, 0.0, 0.25*d2, 0.0]
@@ -27,8 +27,22 @@ def populate_matrix(A, u, t, dx, nx):
             A[2*i][2*i-2:2*i+4] = [-0.25*d1, 0.0, 0.0, 0.0, 0.25*d2, 0.0]
             A[2*i+1][2*i-2:2*i+4] = [-0.5*d1, -0.25*d1, -2.0, 0.0, 0.5*d2, 0.25*d2]
 
+def populate_vector(v, u, x, t, dx, dt, nx):
+    for i in range(nx):
+        d1 = dk(u[i], x[i])*dx - 2.0*k(u[i], x[i])
+        d2 = dk(u[i], x[i])*dx + 2.0*k(u[i], x[i])
+        
+        if i == 0:
+            v[0] = v[1] = (4.0*u[0] - d2*u[1])/dt
+        elif i == nx - 1:
+            v[-2] = v[-1] = (4.0*u[-1] + d1*u[-2])/dt
+        else:
+            v[2*i] = v[2*i+1] = (4.0*u[i] + d1*u[i-1] - d2*u[i+1])/dt
+    
+
+
 # initial data
-u0 = [np.sin(np.pi*2.0*x/(nx + 1.0)) + np.cos(np.pi*3.0*x/(nx + 1.0)) for x in range(1, nx+1)]
+u0 = np.array([np.sin(np.pi*2.0*xi/(nx + 1.0)) + np.cos(np.pi*3.0*xi/(nx + 1.0)) for xi in range(1, nx+1)])
 
 a = 0.0
 b = 1.0
@@ -36,8 +50,8 @@ ua = 0.0
 ub = 0.0
 u = u0
 dx = (b-a)/(nx+1)
-x = [a] + [a + m*dx for m in range(1, nx+1)] + [b]
-lim = [min([ua, ub]+u0), max([ua, ub]+u0)]
+x = np.array([a + m*dx for m in range(1, nx+1)])
+lim = [min([ua, ub]+u0.tolist()), max([ua, ub]+u0.tolist())]
 m = -1.0 - 2.0*dx*dx/dt
 t = 0
 
@@ -46,11 +60,12 @@ A = np.zeros([2*nx, 2*nx])
 v = np.zeros(2*nx)
 for i in range(nt):
     # set up the matrix for each step of the solution
-    populate_matrix(A, u, t, dx, nx)
+    populate_matrix(A, u, x, t, dx, nx)
 
     # construct the RHS vector 
+    populate_vector(v, u, x, t, dx, dt, nx)
 
-    # approximate the solution iteratively
+    # approximate the slopes for the RK step
     l = np.zeros(2*nx)
     l_prev = np.zeros(2*nx)
     e = 1.0
@@ -58,5 +73,17 @@ for i in range(nt):
         l_prev = l
         l = (v - np.dot(A, l))/m
         e = np.linalg.norm(l - l_prev)
+
+    # update the solution
+    for j in range(nx):
+        u[j] += dt*0.5*(l[2*j] + l[2*j+1])
+    
+
+    # plot the solution
+    if i % 50 == 0:
+        plt.plot([a] + x.tolist() + [b], [ua]+u.tolist()+[ub], label='t = {}'.format(t))
+        plt.legend()
+        plt.ylim(lim)
+        plt.show()
 
     t += dt
