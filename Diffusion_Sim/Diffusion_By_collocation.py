21c9813a6f1dca209b2398bd977e9b01455e3d65
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2024-04-23 21:46:11 -0500

    Add files via upload

---
+++ Diffusion_Sim/Diffusion_By_collocation.py
@@ -0,0 +1,55 @@
+import numpy as np
+import scipy.linalg as la
+import matplotlib.pyplot as plt
+
+def Gram_Schmidt_Orthonormalization(n, a, b):
+    # Generate the n orthogonal polynomials using the Gram-Schmidt orthonormalization process
+
+    L = [np.polynomial.Polynomial([1])]
+    # generate the orthogonal polynomials   
+    for i in range(1, n):
+        p = np.polynomial.Polynomial([0, 1])
+        num = (p*L[i-1]*L[i-1]).integ()
+        den = (L[i-1]*L[i-1]).integ()
+        p[0] = (num(b) - num(a))/(den(b) - den(a))
+            
+        L.append(p)
+
+    # find the roots of the highest order polynomial (these will be the collocation points)
+    x = L[-1].roots()
+
+    # return the Legendre polynomials and the collocation points
+    return L, x
+
+
+def populate_matrix(A, x, dt, nx, k, dk, L, dL, ddL):
+
+    for i in range(nx):
+        for j in range(nx):
+            A[i][j] = np.polyval(L[j], x[i]) - dt*[np.polyval(dL[j], x[i]) + np.polyval(ddL[j], x[i])]
+            
+
+def diffusion_sim(nt, nx, dt, u0, a, b, ua, ub, k, dk):
+    # Simulate the diffusion equation using the collocation method
+    # note that we use the legendra polynomials as basis functions and the roots of the largest one as collocation points
+    # be careful about the number of collocation points used, collocation generally requires much fewer points than finite differences
+
+    lim = [min([ua, ub]+u0.tolist()), max([ua, ub]+u0.tolist())]
+    u = u0
+    t = 0
+
+    L, x = Gram_Schmidt_Orthonormalization(nx, a, b)
+
+
+    for i in range(nt):
+
+
+
+        # plot the solution
+        plt.cla()
+        plt.plot([a] + x.tolist() + [b], [ua]+u.tolist()+[ub], label='t = {}'.format(t))
+        plt.legend()
+        plt.ylim(lim)
+        plt.pause(0.0001)
+
+        t += dt
