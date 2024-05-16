0cb995eeb1b2eb28972428da3107362db50d054a
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2024-05-15 21:44:15 -0500

    Update Diffusion_By_collocation.py

--- Diffusion_Sim/Diffusion_By_collocation.py
+++ Diffusion_Sim/Diffusion_By_collocation.py
@@ -1,91 +1,99 @@
 import numpy as np
 import numpy.polynomial.polynomial as p
 import scipy.linalg as la
-import scipy.sparse as sp
 import matplotlib.pyplot as plt
 
 
-def Gram_Schmidt_Orthonormalization(n, a=-1, b=1):
-    # Generate the n orthogonal polynomials using the Gram-Schmidt orthonormalization process
-    
-    L = [[1]]
-    # generate the orthogonal polynomials   
-    for i in range(1, n):
-        num = p.polyint(p.polymul([0, 1], p.polymul(L[i-1], L[i-1])))
-        den = p.polyint(p.polymul(L[i-1],L[i-1]))
-        c = (p.polyval(a, num) - p.polyval(b, num))/(p.polyval(b, den) - p.polyval(a, den))
-        L.append(p.polymul([c, 1], L[i-1]))
+def rescale_polynomial(p, a0, b0, a1, b1):
+    # Rescale the polynomial p from the interval [a0, b0] to [a1, b1]
+    s = (b1 - a1) / (b0 - a0)
+    new = []
+    for i in range(len(p)):
+        t = [1]
+        for j in range(i):
+            t = p.polymul(t, [a1 - a0*s, s])
 
-        if i > 1:
-            num = den
-            den = p.polyint(p.polymul(L[i-2],L[i-2]))
-            c = (p.polyval(a, num) - p.polyval(b, num))/(p.polyval(b, den) - p.polyval(a, den))
-            L[-1] = p.polyadd(L[-1], p.polymul([c], L[i-2]))    
- 
-    return L
+        new = p.polyadd(np, p.polymul(p[i], t))
+
+    return new
 
 
 def Chebyshev_Polynomials(n, a=-1, b=1):
     # Generate the n Chebyshev polynomials
-    
-    L = [[1]]
-    L.append([0, 1])
-    for i in range(2, n):
-        L.append(p.polysub(p.polymul([0, 2], L[i-1]), L[i-2]))
-    
+    for i in range(n):
+        if i == 0:
+            L = [np.array([1])]
+        elif i == 1:
+            L.append(np.array([0, 1]))
+        else:
+            L.append(p.polysub(p.polymul([0, 2], L[i-1]), L[i-2]))
+
+    if a != -1 or b != 1:
+        for i in range(n):
+            L[i] = rescale_polynomial(L[i], -1, 1, a, b)
+
     return L
 
 
-def populate_matrix(M, N, x, ua, ub, nx, k, dk, L, dL, ddL):
-
-    for i in range(nx):
-        for j in range(nx):
-            if i == 0:
-                N[i][j] = ua
-            elif i == nx-1:
-                N[i][j] = ub
+def generate_matrix(n, x, L, dL, dt, ua, ub):
+    A = np.zeros((n, n))
+    B = np.zeros((n, n))
+    for i in range(n):
+        for j in range(n):
+            if i==0:
+                B[i][j] = ua
+            elif i==n-1:
+                B[i][j] = ub
             else:
-                N[i][j] = (p.polyval(x[i], dL[j])*dk(x[i]) + p.polyval(x[i], ddL[j])*k(x[i]))
+                B[i, j] = p.polyval(x[i], dL[j])
 
-            M[i][j] = p.polyval(x[i], L[j])
-                        
-
-def diffusion_sim(nt, nx, dt, u0, a, b, k, dk, ua=0, ub=0):
-    # Simulate the diffusion equation using the collocation method
-    # note that we use the legendra polynomials as basis functions and the roots of the largest one as collocation points
-    # be careful about the number of collocation points used, collocation generally requires much fewer points than finite differences
-
-    M = np.zeros([nx, nx])
-    N = np.zeros([nx, nx])
-    lim = [-1, 1]
-    t = 0
+            A[i, j] = p.polyval(x[i], L[j])
     
-    #L = Gram_Schmidt_Orthonormalization(nx)
-    L = Chebyshev_Polynomials(nx)
-    x = [a + (b-a)/(nx-1)*i for i in range(nx)]
-    w = np.array([[p.polyval(x[i], L[j]) for j in range(nx)] for i in range(nx)])
+    return A, B
 
-    u = [ua] + [np.sin(np.pi*i) for i in x[1:-1]] + [ub]
-    dL = [[0]] + [np.polyder(poly) for poly in L[1:]]
-    ddL = [[0], [0]] + [np.polyder(poly) for poly in dL[2:]]
 
-    populate_matrix(M, N, x, ua, ub, nx, k, dk, L, dL, ddL)
+def rk4_step(A, c, dt):
+    k1 = A.dot(c)
+    k2 = A.dot(c + 0.5*dt*k1)
+    k3 = A.dot(c + 0.5*dt*k2)
+    k4 = A.dot(c + dt*k3)
+    return c - dt*(k1 + 2*k2 + 2*k3 + k4)/6 
 
+
+def diffusion_sim(nt, nx, dt, u0, a=-1, b=1, ua=0, ub=0):
+
+    x = [b] + [np.cos((2*i+1)*np.pi/(2*(nx-2))) for i in range(nx-2)] + [a]
+    x.reverse()
+    print(x)
+    u = [ua] + [u0(x[i]) for i in range(1, nx-1)] + [ub]
+
+    T = Chebyshev_Polynomials(nx, a, b)
+    ddT = [p.polyder(p.polyder(T[i])) for i in range(nx)]
+
+    # Generate the matrices
+    A, B = generate_matrix(nx, x, T, ddT, dt, ua, ub)
+    c = la.solve(A, u)
+
+    #B = np.dot(la.inv(A),B)
+    lim = [min(u), max(u)]
+    print(B)
     for i in range(nt):
-
-        # solve for the collocation coefficients
-        a = sp.linalg.gmres(M+dt*N, u)[0]
-        # reconstruct the solution
-        u = np.dot(M, a)
-        
-        # plot the solution
-        #if i % 100 == 0:
-        if True:
+        if i % 1000 == 0:
+            y = np.linspace(a, b, 100)
+            z = [np.sum([c[j]*p.polyval(y[i], T[j]) for j in range(nx)]) for i in range(100)]
             plt.cla()
-            plt.plot(list(x), u.tolist(), label='t = {}'.format(t))
-            plt.legend()
             plt.ylim(lim)
-            plt.pause(0.03)
+            plt.plot(y, z)
+            plt.plot(x, u, 'ro')
+            plt.pause(0.1)
 
-        t += dt
+        c = rk4_step(B, c, dt)
+        u = A.dot(c)
+        #u = rk4_step(B, u, dt)
+
         
+
+def u0(x):
+    return x
+
+diffusion_sim(1000000, 7, 0.000001, u0, -1, 1, 0, 0)
