7a9d7d3605dc9eedd0a69b139a8fecfb2401ae1a
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2025-01-24 21:25:12 -0600

    Update nonlinear_heat.py

--- nonlinear-heat/nonlinear_heat.py
+++ nonlinear-heat/nonlinear_heat.py
@@ -8,14 +8,24 @@ def F(u, prev, c, k, dk):
     f = np.zeros(len(u))
 
     for i in range(len(u)):
+        # du and ddu represent the differences used to approximate derivatives
         if i==0:
-            f[i] = prev[i] - u[i] - c*( 0.25*dk(u[i]) * (u[i+1])**2 + k(u[i]) * ( - 2*u[i] + u[i+1]) )
+            du = u[i+1]
+            ddu = ( - 2*u[i] + u[i+1])
+
+            f[i] = prev[i] - u[i] - c*( 0.25*dk(u[i]) * du**2 + k(u[i]) * ddu )
             
         elif i==len(u)-1:
-            f[i] = prev[i] - u[i] - c*( 0.25*dk(u[i]) * ( - u[i-1])**2 + k(u[i]) * (u[i-1] - 2*u[i]) )
+            du = -u[i-1]
+            ddu = (u[i-1] - 2*u[i])
+
+            f[i] = prev[i] - u[i] - c*( 0.25*dk(u[i]) * du**2 + k(u[i]) * ddu )
             
         else:
-            f[i] = prev[i] - u[i] - c*( 0.25*dk(u[i]) * (u[i+1] - u[i-1])**2 + k(u[i]) * (u[i-1] - 2*u[i] + u[i+1]) )
+            du = (u[i+1] - u[i-1])
+            ddu = (u[i-1] - 2*u[i] + u[i+1])
+
+            f[i] = prev[i] - u[i] - c*( 0.25*dk(u[i]) * du**2 + k(u[i]) * ddu )
     
     return f
 
@@ -26,22 +36,32 @@ def generate_jacobian(u, c, k, dk, ddk):
     J = np.zeros([n, n])
 
     for i in range(n):
+        # du and ddu represent the differences used to approximate derivatives
         if i==0:
-            J[i][i] = 1 - c*(0.25*ddk(u[i])*(u[i+1]) + dk(u[i])*( - 2*u[i] + u[i+1]) - 2*k(u[i]))
+            du = u[i+1]
+            ddu = ( - 2*u[i] + u[i+1])
+
+            J[i][i] = 1 - c*(0.25*ddk(u[i])*du + dk(u[i])*ddu - 2*k(u[i]))
             
-            J[i][i+1] = -c*(0.5*dk(u[i])*(u[i+1]) + k(u[i]))        
+            J[i][i+1] = -c*(0.5*dk(u[i])*du + k(u[i]))        
 
         elif i==n-1:
-            J[i][i-1] = c*(0.5*dk(u[i])*( - u[i-1]) - k(u[i]))
+            du = -u[i-1]
+            ddu = (u[i-1] - 2*u[i])
 
-            J[i][i] = 1 - c*(0.25*ddk(u[i])*( - u[i-1]) + dk(u[i])*(u[i-1] - 2*u[i]) - 2*k(u[i]))
+            J[i][i-1] = c*(0.5*dk(u[i])*du - k(u[i]))
+
+            J[i][i] = 1 - c*(0.25*ddk(u[i])*du + dk(u[i])*ddu - 2*k(u[i]))
             
         else:
-            J[i][i-1] = c*(0.5*dk(u[i])*(u[i+1] - u[i-1]) - k(u[i]))
+            du = (u[i+1] - u[i-1])
+            ddu = (u[i-1] - 2*u[i] + u[i+1])
 
-            J[i][i] = 1 - c*(0.25*ddk(u[i])*(u[i+1] - u[i-1]) + dk(u[i])*(u[i-1] - 2*u[i] + u[i+1]) - 2*k(u[i]))
+            J[i][i-1] = c*(0.5*dk(u[i])*du - k(u[i]))
+
+            J[i][i] = 1 - c*(0.25*ddk(u[i])*du + dk(u[i])*ddu - 2*k(u[i]))
             
-            J[i][i+1] = -c*(0.5*dk(u[i])*(u[i+1] - u[i-1]) + k(u[i]))
+            J[i][i+1] = -c*(0.5*dk(u[i])*du + k(u[i]))
 
     return J
 
@@ -63,6 +83,21 @@ def newton_step(u, J, c, k, dk):
 
 
 def run_heat_sim(u0, a, b, dt, nx, nt, k, dk, ddk):
+    # inputs:
+    # u0: function defining the initial heat distribution
+    # a: left endpoint
+    # b: right endpoint
+    # dt: timestep 
+    # nx: number of points for spatial discretization
+    # nt: number of timesteps to run for
+    # k: the function specifying the thermal diffusivity
+    # dk: derivative of k
+    # ddk: derivative of dk
+    #
+    # output:
+    # displays the evolution of the solution using matplotlib
+
+
     x = np.linspace(a, b, nx+2, endpoint=True)
     u = np.array([u0(t) for t in x[1:-1]])
     t = 0.0  
@@ -81,13 +116,27 @@ def run_heat_sim(u0, a, b, dt, nx, nt, k, dk, ddk):
         time_label = str(np.round(t, 5))
         ax.clear()
         ax.set_ylim(bottom=lower_bound, top=upper_bound)
-        ax.plot(x, [0] + list(u) + [0], color='b', label='nonlinear: k=0.5*(1 + u^2)')
+        ax.plot(x, [0] + list(u) + [0], color='b', label='nonlinear: k=0.5/(1 + u^2)')
         ax.legend()
         ax.set_title('Time={}'.format(time_label))
         plt.pause(0.01)
 
 
 def heat_compare_gif(u0, a, b, dt, nx, nt, k, dk, ddk, filename='heat_sim.gif'):
+    # inputs:
+    # u0: function defining the initial heat distribution
+    # a: left endpoint
+    # b: right endpoint
+    # dt: timestep 
+    # nx: number of points for spatial discretization
+    # nt: number of timesteps to run for
+    # k: the function specifying the thermal diffusivity
+    # dk: derivative of k
+    # ddk: derivative of dk
+    #
+    # output:
+    # creates a gif of the evolution of the normal heat equation and the given nonlinear variant
+
     x = np.linspace(a, b, nx+2, endpoint=True)
     u = np.array([u0(t) for t in x[1:-1]])
     t = 0.0
@@ -121,7 +170,7 @@ def heat_compare_gif(u0, a, b, dt, nx, nt, k, dk, ddk, filename='heat_sim.gif'):
 
         ax.clear()
         ax.set_ylim(bottom=lower_bound, top=upper_bound)
-        ax.plot(x, [0] + list(u) + [0], color='b', label='nonlinear: k=0.5*(1 + u^2)')
+        ax.plot(x, [0] + list(u) + [0], color='b', label='nonlinear: k=0.5/(1 + u^2)')
         ax.plot(x, [0] + list(baseline) + [0], color='r', label='linear: k=1')
         ax.legend()
         ax.set_title('Time={}'.format(time_label))
