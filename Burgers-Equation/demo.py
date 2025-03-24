7f28949d3ac61873e0fc5332bab4708474d19177
danielennis521 <125907715+danielennis521@users.noreply.github.com>
2025-03-24 16:21:29 -0500

    Update demo.py

--- Burgers-Equation/demo.py
+++ Burgers-Equation/demo.py
@@ -10,9 +10,11 @@ def u0(x):
     return t1 - t2
 
 
+n=10
 test = b.Burger_Sim(-6, 6, 200, 0.002, 0)
+colors = []
 
-for i in range(10):
-    test.add_sim(lambda x, i=i: 1/(0.15*i*x**2 + 1), 5, u0)
+for i in range(n):
+    test.add_sim(lambda x, i=i: 1/(0.15*i*x**2 + 1), 5, u0, color=(0, 0.75 - 0.75*i/n, i/n))
 
-test.play(1000)
+test.create_gif(n=200, filename='burger_eq_near_shock.gif')
